e090074233a11bbaaab99d7981cbd148a81d2285
Sarah Schüßler <stschuessler@googlemail.com>
2021-09-19 13:04:33 +0200

    Access data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Header from './app_components/header_component/Header'
 import Card from './app_components/card_component/Card'
 import Footer from './app_components/footer_component/Footer'
 
-function App(data) {
+function App({ data }) {
+  console.table(data)
+  console.log(data[0].name)
+
   return (
     <div className="App">
       <Header />
