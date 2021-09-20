e1389418550f6249d8cf9911789945d938448db7
Sarah Schüßler <stschuessler@googlemail.com>
2021-09-20 16:17:57 +0200

    Import useState

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './app_components/header_component/Header'
 import Card from './app_components/card_component/Card'
 import Footer from './app_components/footer_component/Footer'
 import CreateCharacterForm from './app_components/Form'
+import { useState } from 'react'
 
 function App({ data }) {
   let allData = data
