2d37c9967de5e1ddbf949836e5a80b766ce22e45
Sarah Schüßler <stschuessler@googlemail.com>
2021-09-20 14:25:46 +0200

    Import CreateCharacterForm function

--- src/app_components/footer_component/Footer.js
+++ src/app_components/footer_component/Footer.js
@@ -1,4 +1,5 @@
 import './Footer.css'
+import CreateCharacterForm from '../Form'
 
 function Footer() {
   return (
@@ -6,6 +7,7 @@ function Footer() {
       <button>species</button>
       <button>episodes</button>
       <button>status</button>
+      {CreateCharacterForm()}
     </footer>
   )
 }
