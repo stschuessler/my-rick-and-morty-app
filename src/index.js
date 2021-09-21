6c79e7a99dbd8e8f961574377bd45afa74966b14
Sarah Schüßler <stschuessler@googlemail.com>
2021-09-21 15:12:59 +0200

    Invoke GlobalStyles style component

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App data={data} />
   </React.StrictMode>,
   document.getElementById('root'),
