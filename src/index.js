7f0f2231c1b7eac0d9f85b526f78a88909a03392
Sarah Schüßler <stschuessler@googlemail.com>
2021-09-21 15:13:21 +0200

    Import GlobalStyles to file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 import data from './rick-and-morty.json'
 import reportWebVitals from './reportWebVitals'
+import GlobalStyles from './GlobalStyles'
 
 ReactDOM.render(
   <React.StrictMode>
